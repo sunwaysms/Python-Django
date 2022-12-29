a4d593cddb33500c0504b97a24999d6fbfa4963c
Abraham Shad <shadafrough@gmail.com>
2022-12-29 13:46:46 +0330

    1.1.0

--- SunwayConfig/sms/urls.py
+++ SunwayConfig/sms/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('getcredit/', views.getcredit, name='getcredit'),
+    path('sendsms/', views.sendsms, name='sendsms'),
 ]
--- SunwayConfig/sms/views.py
+++ SunwayConfig/sms/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
+from zeep import Client
+from django.http import HttpResponse
 
-# Create your views here.
+
+# SunWaySMS Webservice Username and Password (https://sunwaysms.com/free/)
+
+wsdl = 'https://sms.sunwaysms.com/SMSWS/SOAP.asmx?wsdl'
+
+client = Client(wsdl=wsdl)
+
+array_of_string_type = client.get_type("ns0:ArrayOfString")
+array_of_long_type = client.get_type("ns0:ArrayOfLong")
+
 def index(request):
-    return render(request,'index.html')
+    return render(request,'base.html')
+
+
+def getcredit(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        a = client.service.GetCredit(username, password)
+        return HttpResponse(f'<p>موجودی حساب شما در پنل سان وی {a} ریال </p> <a href="http://127.0.0.1:8000">برگشت</a>')
+    
+    return render(request, 'getcredit.html')
+
+
+def sendsms(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        RecipientNumber = request.POST.get('RecipientNumber1')
+        RecipientNumber2 = array_of_string_type([RecipientNumber])
+        MessageBody = request.POST.get('MessageBody')
+        SpecialNumber = request.POST.get('SpecialNumber')
+        c = client.service.SendArray(username, password, RecipientNumber2, MessageBody, SpecialNumber)
+        return HttpResponse(f'<p>عملیات انجام شد کد پیگیری {c}</p> <a href="http://127.0.0.1:8000">برگشت</a>')
+    return render(request,'sendsms.html')
